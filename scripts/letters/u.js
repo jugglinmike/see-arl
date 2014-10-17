7e2b5bf9bb66696fc13f28f188aee2f583b70c0e
Jenn Schiffer <jenn@pancaketheorem.com>
2014-10-17 10:06:55 -0400

    the letter uuuuuuuu

--- scripts/letters/u.js
+++ scripts/letters/u.js
@@ -1,5 +1,23 @@
-define(function() {
-	return function(elem) {
-		elem.innerHTML = 'u';
-	};
+// stars
+require.config({ paths: { "jquery": "http://code.jquery.com/jquery" } });
+
+define(function(require) {
+  var $ = require("jquery").noConflict();
+
+  return function(elem) {
+    var $elem = $(elem);
+
+    $elem.html('');
+
+    for ( var i = 0; i < 10; i++ ) {
+      var $u = $('<span class="uuu">u</span>');
+      $u.css({
+        opacity: '.'+ i,
+        display: 'block'
+      });
+
+      $elem.append($u);
+    }
+
+  };
 });
