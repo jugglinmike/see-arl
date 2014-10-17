829868e3aad6c988459c51249e6586906396eaad
Matt Surabian <matt@mattsurabian.com>
2014-10-17 11:48:41 -0400

    The letter P

--- scripts/letters/p.js
+++ scripts/letters/p.js
@@ -1,5 +1,37 @@
-define(function() {
+require.config({ paths: { "jquery": "http://code.jquery.com/jquery" } });
+
+define(function(require) {
+  var $ = require("jquery").noConflict();
+
 	return function(elem) {
-		elem.innerHTML = 'p';
+    var $elem = $(elem);
+
+    $elem.css({
+      position: 'relative',
+      width: '245px',
+      height: '165px',
+      color: 'white',
+      fontSize: '10px',
+      cursor: 'pointer',
+      background: 'url(http://www.reactiongifs.com/r/cwpt.gif)'
+    });
+
+    $elem.on('click', function() {
+      alert('Kid in the background going fuckin\' crazyyyyy!')
+    });
+
+    var $letter = $('<div>p</div>');
+    $elem.append($letter);
+
+    setInterval(function() {
+      $letter.css({
+        position: 'absolute',
+        fontSize: Math.ceil(Math.random()*6)*18 + 'px',
+        left: Math.ceil(Math.random()*200) + 'px',
+        top: Math.ceil(Math.random()*40) + 'px'
+      })
+    },500)
 	};
 });
+
+
