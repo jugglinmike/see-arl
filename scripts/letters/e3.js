dfe966facf0e2ddb626f2044a8e2c1f91cbf7473
bobholt <bobholt@gmail.com>
2014-10-17 14:09:27 -0400

    add e3

--- scripts/letters/e3.js
+++ scripts/letters/e3.js
@@ -1,5 +1,22 @@
 define(function() {
 	return function(elem) {
-		elem.innerHTML = 'e';
+		elem.style.backgroundImage = 'url(assets/e3.gif)';
+    elem.style.width = '152px';
+    elem.style.height = '160px';
+    elem.style.overflow = 'hidden';
+    elem.style.fontSize = '11px';
+    elem.innerHTML = 'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n' +
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n' +
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n' +
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n' +
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n' +
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n' +
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n' +
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n' +
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n' +
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n' +
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n' +
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n' +
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
 	};
 });
