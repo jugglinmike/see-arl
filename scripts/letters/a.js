3698b484a0fd2ebfb580ea1b79568bc0b29d6d95
Isaac Durazo <isaacdurazo@gmail.com>
2014-10-17 11:34:28 -0400

    Letter a

--- scripts/letters/a.js
+++ scripts/letters/a.js
@@ -1,5 +1,11 @@
-define(function() {
-	return function(elem) {
-		elem.innerHTML = 'a';
-	};
+
+require.config({ paths: { 'jquery': 'http://code.jquery.com/jquery' } });
+define(function(require) {
+  var $ = require('jquery').noConflict();
+
+  return function(elem) {
+    $(elem).addClass('letter-a');
+
+    elem.innerHTML = 'a';
+  };
 });
