d7a0e8b620ab8f3d0d6e04a7a1edee3eef3dc24c
Boaz Sender <boaz@bocoup.com>
2014-10-17 16:23:22 -0400

    GGGG >>> WE MISS YOU CARL

--- scripts/letters/g1.js
+++ scripts/letters/g1.js
@@ -1,5 +1,20 @@
 define(function() {
 	return function(elem) {
 		elem.innerHTML = 'G';
+    window.BOAZSLETTERISGLOABAWL = elem;
+
+    elem.style.color = '#ff69b4';
+    elem.style.width = '30px';
+    elem.style.height = '240px';
+
+    setInterval(function(){
+      var size = elem.style.width.substring(0, elem.style.width.length - 2);
+      size++;
+
+      elem.style.width = size + "px";
+
+    }, 100);
+
+    elem.style.background = "url('http://www.princessfonts.com/images/headers/letterG.gif') 10px -10px"
 	};
 });
