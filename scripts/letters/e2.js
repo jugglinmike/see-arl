dec96794be51128094c07126053d4fda6d50b22d
deathbearbrown <deathbearbrown@gmail.com>
2014-10-17 10:16:01 -0400

    Sue and Claire!  M & e2

--- scripts/letters/e2.js
+++ scripts/letters/e2.js
@@ -1,5 +1,10 @@
 define(function() {
-	return function(elem) {
-		elem.innerHTML = 'e';
-	};
+  return function (elem) {
+    var videoUrl = "https://vine.co/v/OqYFYVTgM6z/embed/simple";
+    var iframe = document.createElement("IFRAME");
+        iframe.setAttribute("src", videoUrl);
+        iframe.style.width = 150+"px";
+        iframe.style.height = 150+"px";
+    elem.appendChild(iframe);
+  };
 });
