96fc8726b46b4c3fd6b16e2a78512f96064e174a
adam j. sontag <ajpiano@ajpiano.com>
2014-10-17 12:01:08 -0400

    Add letter G2, like a G6

--- scripts/letters/g2.js
+++ scripts/letters/g2.js
@@ -1,5 +1,34 @@
-define(function() {
-	return function(elem) {
-		elem.innerHTML = 'g';
-	};
+require.config({ paths: { 'jquery': 'http://code.jquery.com/jquery' } });
+  define(function(require) {
+    var $ = require('jquery').noConflict();
+
+    return function(elem) {
+      var $el = $(elem)
+      .addClass('color-party letter-g2')
+      .html('g');
+
+      function downhome() {
+        $el.addClass('huge').html("'");
+        setTimeout(function(){
+          $el.toggleClass("huge medium");
+          setTimeout(reset, 1500);
+        },1500);
+      }
+
+      function reset() {
+        $el.html("g");
+        setTimeout(function() {
+          $el.removeClass("huge medium");
+          setTimeout(downhome, 1500);
+        });
+      }
+
+      setTimeout(downhome,1500);
+
+      setInterval(function() {
+        $(elem).css('color', window.randomColor);
+      }, 500);
+      
+    };
+
 });
