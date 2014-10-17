56177a02e8c7b06dc8091c44211da75d76db3543
bmac <bmac325@GMail.com>
2014-10-17 13:00:54 -0400

    add animation for c1

--- scripts/letters/c1.js
+++ scripts/letters/c1.js
@@ -1,5 +1,15 @@
 define(function() {
-	return function(elem) {
-		elem.innerHTML = 'c';
-	};
+  return function(elem) {
+    function restartAnimation() {
+      elem.classList.remove('c1');
+      setTimeout(function() {
+        elem.classList.add('c1');
+      }, 1000 * 10);
+    }
+
+    elem.innerHTML = 'c';
+    elem.classList.add('c1');
+
+    elem.addEventListener('animationend', restartAnimation);
+  };
 });
