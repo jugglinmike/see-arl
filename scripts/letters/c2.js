5efe76eeab1166070a04991953b08ea65349ddf3
Greg Smith <gsmith@incompl.com>
2014-10-17 15:07:38 -0400

    greg's letter c

--- scripts/letters/c2.js
+++ scripts/letters/c2.js
@@ -1,5 +1,21 @@
 define(function() {
 	return function(elem) {
-		elem.innerHTML = 'C';
+		elem.style.fontFamily = 'monospace';
+		elem.style.whiteSpace = 'pre';
+		elem.innerHTML = '_______________________\n' +
+		                 '_________CCCCCCCCC_____\n' +
+		                 '______CCCCCCCCCCCC_____\n' +
+		                 '____CCCCCCCCCCCCCC_____\n' +
+		                 '___CCCCCCCCCCCCCCC_____\n' +
+		                 '___CCCCCCCC____________\n' +
+		                 '__CCCCCCC______________\n' +
+		                 '__CCCCCC_______________\n' +
+		                 '__CCCCCC_______________\n' +
+		                 '___CCCCCC______________\n' +
+		                 '___CCCCCCCCCCCCCCC_____\n' +
+		                 '____CCCCCCCCCCCCCC_____\n' +
+		                 '______CCCCCCCCCCC______\n' +
+		                 '_______________________';
 	};
 });
+
